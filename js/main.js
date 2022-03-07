c9247e6a64f223a8352c18e864319ae8e1e1a9b3
SimoneScapin <simonescap87@gmail.com>
2022-03-07 15:33:44 +0100

    aggiunta classe che colora i quadrati

--- js/main.js
+++ js/main.js
@@ -18,17 +18,35 @@ button.addEventListener('click', function(){
     console.log(nBox);
 
     boxSize = Math.sqrt(nBox);
+    wrapper.innerHTML = "";
 
     for (let i = 1; i<=nBox; i++){
         let newDiv = document.createElement('div');
+        newDiv.addEventListener('click', function(){
+            newDiv.classList.add('clicked')
+        })
+        
+        /** stai creando un elemento nuovo che poi aggiungerai (r34) al suo parent */
+        // creare un sistema di ascolto sul singolo elemento che ne colori il contenuto
+        // "colorare il contenuto" => aggiungere una classe
+
         newDiv.classList.add('box');
         newDiv.style.width = `calc(100% / ${boxSize})`
         newDiv.style.height = newDiv.style.width;
         newDiv.innerText = i;
+
+        // qui devi implementare il sistema di "aggiunta" della classe quando avviene uno specifico evento di click
+
         wrapper.append(newDiv);
     }
 })
 
+/*
+A implemento la funzionalita del singolo quadrato che si colora al click
 
+B realizzare la lista delle bombe in base alla difficoltà 
+
+C cambio la classe che gli devo attribuire con l'eventListener vedi punto A in base a se sia una bomba o no
+*/
 
 
