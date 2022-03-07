2dcbe1eeabe2268018f22e00ed5c4cee66dc54a9
SimoneScapin <simonescap87@gmail.com>
2022-03-07 14:13:52 +0100

    Aggiunta dei Box Petricca's Style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 const button = document.getElementById('play');
-const wrapper = document.getElementById('my-wrapper')
+const wrapper = document.getElementById('my-wrapper');
+const scelta = document.getElementById('difficolta');
+let sceltaUtente = scelta.value;
+let nBox = 0;
+
 
 button.addEventListener('click', function(){
-    for  (let i = 1 ; i<=100  ; i++){
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('box');
-    newDiv.innerText = i;
-    wrapper.append(newDiv);
+    if ( scelta.value == 'easy'){
+        nBox=100;   
+        console.log('easy')
+    } else if (scelta.value =='medium'){
+        nBox=81;
+        console.log('medium')
+    }else{
+        nBox=49;
+        console.log('high');   
+    }
+    console.log(nBox);
+
+    boxSize = Math.sqrt(nBox);
+
+    for (let i = 1; i<=nBox; i++){
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('box');
+        newDiv.style.width = `calc(100% / ${boxSize})`
+        newDiv.style.height = newDiv.style.width;
+        newDiv.innerText = i;
+        wrapper.append(newDiv);
     }
 })
